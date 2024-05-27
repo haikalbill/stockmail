b79d186c40c2a0a237754b6b7dffbc1b612d42fa
haikalbill <90669152+haikalbill@users.noreply.github.com>
2024-05-28 03:10:49 +0800

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -6,6 +6,9 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+from dateutil.parser import parse
+
+
 
 def main():
     # Define START and TODAY dates
