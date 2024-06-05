b48bfbd9c2e68171263bd54157de1e20de74591a
haikalbill <90669152+haikalbill@users.noreply.github.com>
2024-06-05 08:41:01 +0800

    Update stockmail.py

--- stockmail.py
+++ stockmail.py
@@ -96,6 +96,7 @@ def main():
         stock = st.selectbox("Select a Stock", stock_ticker_list)
         stock_ticker = yf.Ticker(stock)
         stock_name = stock_ticker.info["longName"]
+        stock_ticker_list.remove('ADBE')
 
 
     ##timeframe##
